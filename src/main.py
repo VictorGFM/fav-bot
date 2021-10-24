3b745a9ad60cfbda985a77faba0bdb094aa7cb07
Victor Moraes <victorgabrielvgfm@gmail.com>
2021-10-23 21:32:22 -0300

    Removes unnecessary server

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from decouple import config
 
-from server import server
 from clients.discord_client import *
 
 disc_client = DiscordClient()
-server.keep_alive()
 disc_client.run(config("TOKEN"))
